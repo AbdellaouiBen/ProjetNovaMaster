74a0286837875056fb9e920d5cbc6da5cd382f75
AbdellaouiBen <benbillal@hotmail.com>
2020-04-17 09:08:17 +0200

    +- prêt pour présentation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 @section('content_header')
     <h1 class="m-0 text-dark">Dashboard</h1>
 @stop
-
+ 
 @section('content')
     <div class="row">
         <div class="col-12">
@@ -13,22 +13,11 @@
                 <div class="card-body">
                     <p class="mb-0">You are logged in as {{Auth::user()->name}}!</p>
                 </div>
-            </div>
+            </div>  
         </div>
-    </div>
+    </div> 
+ 
 
-<div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">New Members</span>
-              <span class="info-box-number">2,000</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
   
 @stop
  
